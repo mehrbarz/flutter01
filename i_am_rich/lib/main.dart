0a960e91aca6faa2ec266cb36819c55a07a0e155
mehrbarz <mehrbarzegar@gmail.com>
2021-03-05 18:13:34 +0100

    reformat with dartfmt

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text('Mehr')));
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Mehr'),
+      ),
+    ),
+  );
 }
-
-
