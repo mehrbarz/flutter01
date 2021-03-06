5f12e401ffc1ab79a97dde27ad169a011496286d
mehrbarz <mehrbarzegar@gmail.com>
2021-03-06 12:25:36 +0100

    NetworkImage

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Mehr'),
+      home: Scaffold(
+        backgroundColor: Colors.lightGreen,
+        appBar: AppBar(
+          title: Text('I am rich'),
+          backgroundColor: Colors.teal[800],
+        ),
+        body: Center(
+          child: Image (
+            image: NetworkImage('https://alanmajchrowicz.com/wp-content/uploads/2016/08/glacier_peak_image_lake_north_cascades_58240.jpg'),
+          ),
+        ),
       ),
     ),
   );
