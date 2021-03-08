f4299231f3e4beb274ee9e79baaa7b1163105e27
mehrbarz <mehrbarzegar@gmail.com>
2021-03-09 00:56:11 +0100

    letterSpacing

--- business_card01/lib/main.dart
+++ business_card01/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,31 +12,30 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.deepPurple,
         body: SafeArea(
-          child: Column(
-            children: [
-              CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage('images/mehr.jpg'),
-              ),
-              Text(
-                'Mehr Barz',
-                style: TextStyle(
+          child: Column(children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('images/mehr.jpg'),
+            ),
+            Text(
+              'Mehr Barz',
+              style: TextStyle(
                   fontFamily: 'Maven Pro',
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white),
-              )
-              ,
-              Text(
-                'Developer',
-                style: TextStyle(
-                    fontFamily: 'Dancing Script',
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.lightGreen),
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            Text(
+              'Developer',
+              style: TextStyle(
+                fontFamily: 'Dancing Script',
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.lightGreen,
+                letterSpacing: 2.6,
               ),
-            ]
-          ),
+            ),
+          ]),
         ),
       ),
     );
