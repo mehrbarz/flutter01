39d7d598cdf6499f9089b502e8b2a1326a8449e2
mehrbarz <mehrbarzegar@gmail.com>
2021-03-10 15:37:52 +0100

    Icon widget

--- business_card01/lib/main.dart
+++ business_card01/lib/main.dart
@@ -12,7 +12,8 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.deepPurple,
         body: SafeArea(
-          child: Column(children: [
+            child: Column(
+          children: [
             CircleAvatar(
               radius: 50,
               backgroundImage: AssetImage('images/mehr.jpg'),
@@ -35,8 +36,58 @@ class MyApp extends StatelessWidget {
                 letterSpacing: 2.6,
               ),
             ),
-          ]),
-        ),
+            Container(
+              padding: EdgeInsets.all(11.11),
+              color: Colors.deepOrange,
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.phone,
+                    size: 30,
+                    color: Colors.teal.shade200,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text(
+                    '+46 73 269 2135',
+                    style: TextStyle(
+                      color: Colors.lime,
+                      fontFamily: 'Dancing Script',
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(11.11),
+              color: Colors.deepOrange,
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    size: 30,
+                    color: Colors.teal.shade200,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text(
+                    'mehrbarz@barz.com',
+                    style: TextStyle(
+                      color: Colors.lime,
+                      fontFamily: 'Dancing Script',
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        )),
       ),
     );
   }
